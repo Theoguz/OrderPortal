d6c08f8c080132098342c4d28dc3b5a0896a0cae
Oguzz <oguzaslan_203@hotmail.com>
2024-02-02 02:06:41 +0300

    Initial commit

---
+++ src/test/java/com/example/orderportal/OrderPortalApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.orderportal;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OrderPortalApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
