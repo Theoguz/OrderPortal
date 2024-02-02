637d918bc7cd96d263c808d0d2df45957ed4540d
Oguzz <oguzaslan_203@hotmail.com>
2024-02-02 22:40:38 +0300

    Uptade data

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,1 +1,4 @@
 insert into Product (name, description, price, stock) values ('Elma','ElmaProduct',50, 100);
+insert into Product (name, description, price, stock) values ('Armut','ArmutProduct',60, 100);
+
+insert into Customer(name) values ('Ahmet');
