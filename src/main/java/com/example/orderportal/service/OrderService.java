033a631ea3e9a6cba819539fdbbca2b2d2296a4b
Oguzz <oguzaslan_203@hotmail.com>
2024-02-03 23:06:15 +0300

    fix

--- src/main/java/com/example/orderportal/service/OrderService.java
+++ src/main/java/com/example/orderportal/service/OrderService.java
@@ -1,27 +1,56 @@
 package com.example.orderportal.service;
 
+import com.example.orderportal.entity.Cart;
 import com.example.orderportal.entity.Customer;
 import com.example.orderportal.entity.Order;
+import com.example.orderportal.repository.CustomerRepository;
 import com.example.orderportal.repository.OrderRepository;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 public class OrderService {
 
     private final OrderRepository orderRepository;
+    private final CustomerService customerService;
+    private final CartService cartService;
+    private final CustomerRepository customerRepository;
 
-    public OrderService(OrderRepository orderRepository) {
+    public OrderService(OrderRepository orderRepository, CustomerService customerService, CartService cartService, CustomerRepository customerRepository) {
         this.orderRepository = orderRepository;
+        this.customerService = customerService;
+        this.cartService = cartService;
+        this.customerRepository = customerRepository;
     }
 
-    public void PlaceOrder(Order order) {
-        orderRepository.save(order);
+
+    public void PlaceOrder(Long id) {
+        Customer customer = customerRepository.findById(id).orElse(null);
+        Cart cart = cartService.GetCart(id);
+        if (cart == null) {
+            System.out.println("Sepetiniz boş");
+        } else {
+            Order order = new Order();
+            order.setCustomer(customer);
+            order.setOrderTotal(cart.getCartTotal());
+            order.setTotalPrice(cart.getCartTotal());
+            orderRepository.save(order);
+            cartService.EmptyCart();
+        }
+
     }
 
-    public void GetOrderForCode(Order order, String code) {
-    }
-
-    public void GetOrderForCustomer(Order order, Customer customer) {
+    public Order GetOrderForCode(String orderCode) {
+        return orderRepository.findByOrderCode(orderCode);
 
     }
+
+    public List<Order> GetAllOrdersForCustomer() {
+        return orderRepository.findAll();
+
+    }
+
 }
+
+
