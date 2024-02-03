dcc3a0967353952be1100f5792e3080c3c2e1fff
Oguzz <oguzaslan_203@hotmail.com>
2024-02-03 23:05:39 +0300

    price history added

---
+++ src/main/java/com/example/orderportal/entity/PriceHistory.java
@@ -0,0 +1,17 @@
+package com.example.orderportal.entity;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.ManyToOne;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class PriceHistory extends BaseEntity {
+
+    @ManyToOne
+    private Product product;
+}
