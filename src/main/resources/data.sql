ec1c3e1f70032cf52cdfa40ce857c4b5de8b4f2f
Oguzz <oguzaslan_203@hotmail.com>
2024-02-03 23:05:19 +0300

    insert some items

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,4 +1,7 @@
 insert into Product (name, description, price, stock) values ('Elma','ElmaProduct',50, 100);
 insert into Product (name, description, price, stock) values ('Armut','ArmutProduct',60, 100);
+insert into Product (name, description, price, stock) values ('ExampleProduct','ExampleDescription',10.99, 5);
 
 insert into Customer(name) values ('Ahmet');
+insert into Customer(name) values ('Mehmet');
+
