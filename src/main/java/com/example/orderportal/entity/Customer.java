823937b2e3b3d6daec41c7ecf357b11d51858c33
Oguzz <oguzaslan_203@hotmail.com>
2024-02-03 08:16:03 +0300

    Customer Fix

--- src/main/java/com/example/orderportal/entity/Customer.java
+++ src/main/java/com/example/orderportal/entity/Customer.java
@@ -1,6 +1,5 @@
 package com.example.orderportal.entity;
 
-import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.OneToMany;
 import jakarta.persistence.OneToOne;
@@ -17,9 +16,6 @@ import java.util.List;
 public class Customer extends BaseEntity {
 
     private String name;
-    private String address;
-    private String phone;
-    private String city;
 
     @OneToOne
     private Cart cart;
