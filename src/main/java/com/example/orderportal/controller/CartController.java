9d7b337cb34e246e89f9e50d4d70d669d97b84b6
Oguzz <oguzaslan_203@hotmail.com>
2024-02-03 03:49:22 +0300

    CartController Added

---
+++ src/main/java/com/example/orderportal/controller/CartController.java
@@ -0,0 +1,36 @@
+package com.example.orderportal.controller;
+
+import com.example.orderportal.entity.Cart;
+import com.example.orderportal.entity.Product;
+import com.example.orderportal.service.CartService;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping(path = "/api/cart")
+public class CartController {
+
+    private final CartService cartService;
+
+    public CartController(CartService cartService) {
+        this.cartService = cartService;
+    }
+
+//    @PostMapping("/add")
+//    public Cart addCart(@RequestBody String name) {
+//        try {
+//            return cartService.AddProductToCart(name);
+//        } catch (Exception e) {
+//            e.printStackTrace();
+//        }
+//        return null;
+//    }
+
+    @PostMapping("/add")
+    public void addProductToCart(@RequestBody AddToCartRequest request) {
+        try {
+            cartService.AddProductToCart(request.getCart(), request.getProduct());
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+}
