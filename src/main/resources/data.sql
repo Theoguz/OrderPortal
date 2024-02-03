3b3b1a70aaadc1d9a9bc29f9b45c6278a871df58
Oguzz <oguzaslan_203@hotmail.com>
2024-02-04 02:32:09 +0300

    Stok fix

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,6 +1,6 @@
 insert into Product (name, description, price, stock) values ('Elma','ElmaProduct',50, 100);
 insert into Product (name, description, price, stock) values ('Armut','ArmutProduct',60, 100);
-insert into Product (name, description, price, stock) values ('ExampleProduct','ExampleDescription',10.99, 5);
+insert into Product (name, description, price, stock) values ('ExampleProduct','ExampleDescription',10, 50);
 
 insert into Customer(name) values ('Ahmet');
 insert into Customer(name) values ('Mehmet');
