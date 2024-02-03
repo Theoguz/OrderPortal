284d4d0573667f72a38cf9c266f78a030d328478
Oguzz <oguzaslan_203@hotmail.com>
2024-02-03 08:17:10 +0300

    OrderRepository findbyordercode added

--- src/main/java/com/example/orderportal/repository/OrderRepository.java
+++ src/main/java/com/example/orderportal/repository/OrderRepository.java
@@ -6,4 +6,6 @@ import org.springframework.stereotype.Repository;
 
 @Repository
 public interface OrderRepository extends JpaRepository<Order, Long> {
+    Order findByOrderCode(String orderCode);
+//    Optional<Object> findByCode(String code);
 }
