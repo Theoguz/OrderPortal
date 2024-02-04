0c35bd7c120d9ed2820061248124bbbd800cb387
Oguzz <oguzaslan_203@hotmail.com>
2024-02-04 03:22:27 +0300

    OrderCode Added UUID

--- src/main/java/com/example/orderportal/entity/Order.java
+++ src/main/java/com/example/orderportal/entity/Order.java
@@ -1,27 +1,24 @@
 package com.example.orderportal.entity;
 
-
 import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Data;
-
 import lombok.NoArgsConstructor;
+
+import java.util.UUID;
 
 @Entity
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
 @Table(name = "order_table")
-public class Order extends BaseEntity{
+public class Order extends BaseEntity {
 
-    @GeneratedValue(strategy= GenerationType.IDENTITY)
-    private String orderCode;
+    private String orderCode = UUID.randomUUID().toString();
     private int orderTotal;
     private double totalPrice;
-
     @ManyToOne
     private Customer customer;
-
 
 
 }
